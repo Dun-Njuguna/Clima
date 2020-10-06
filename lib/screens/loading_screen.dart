3db4c452094ffb527570a50e5da59d539ec79a91
Duncan Kariuki <dkariuki@innovexsolutions.co.ke>
2020-10-06 14:05:09 +0300

    initial

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+void getLocation() async {
+  Position position = await getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low, forceAndroidLocationManager: true);
+  print(position);
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // body: Center(
+        //   child: RaisedButton(
+        //     onPressed: () {
+        //       //Get the current location
+        //       getLocation();
+        //     },
+        //     child: Text('Get Location'),
+        //   ),
+        // ),
+        );
+  }
+}
